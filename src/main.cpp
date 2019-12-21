d9e15d497a1aefbaf14d27bef89d8db01954af42
OnAFriday <cristian.fiuba@gmail.com>
2019-12-20 23:53:59 -0300

    nada

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,13 @@
 int main(){
 	//Menu recorridos;
 	//recorridos.iniciarMenu();
-Menu recorrido;
-recorrido.iniciarMenu();
+	Menu recorrido;
+	recorrido.iniciarMenu();
 //	Ciudad caba;
 	//caba.verRedSubte();
 	//caba.verRedTren();
 	//caba.verRedTransporte();
-
+/*comenatrio para commit*/
 	return 0;
 }
 
